3461225969f364fd18c07edf6e2a278d08045f59
Superdog123 <210721@hkis.edu.hk>
2016-06-06 09:43:49 +0800

    COK

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -4,6 +4,15 @@
     });
 });
 
-//Your jquery goes here
+$("#one").animate({right:'4100px'},6000);
+$("#two").animate({right:'4100px'},6000);
+$("#three").animate({right:'4100px'},6000);
+$("#four").animate({right:'4100px'},6000);
+$("#five").animate({right:'4100px'},6000);
+$("#six").animate({right:'4100px'},6000);
+$("#seven").animate({right:'4100px'},6000);
+$("#eight").animate({right:'4100px'},6000);
+$("#nein").animate({right:'4100px'},6000);
+$("#ten").animate({right:'4100px'},6000);
 
 });
