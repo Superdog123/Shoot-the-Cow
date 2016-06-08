880b80ab73def0e50f669a05cb65568e029b4283
Superdog123 <210721@hkis.edu.hk>
2016-06-08 10:37:28 +0800

    blubber

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -11,39 +11,58 @@ $("#eight").animate({left:'4100px'},6000);
 $("#nein").animate({right:'4100px'},6000);
 $("#ten").animate({left:'4100px'},6000);
 
+var score=0;
+
+if score==10{
+alert("you win!!")
+};
+else{};
 
 $("#one").click(function(){
     $("#one").hide();
+    score++;
 });
 $("#two").click(function(){
     $("#two").hide();
+    score++;
 });
 $("#three").click(function(){
     $("#three").hide();
+    score++;
 });
 $("#four").click(function(){
     $("#four").hide();
+    score++;
 });
 $("#five").click(function(){
     $("#five").hide();
+    score++;
 });
 $("#six").click(function(){
     $("#six").hide();
+    score++;
 });
 $("#seven").click(function(){
     $("#seven").hide();
+    score++;
 });
 $("#eight").click(function(){
     $("#eight").hide();
+    score++;
 });
 $("#nein").click(function(){
     $("#nein").hide();
+    score++;
 });
 $("#ten").click(function(){
     $("#ten").hide();
+    score++;
 });
 
 });// doc.ready
+
+var score=0;
+score++;
 
 
 
